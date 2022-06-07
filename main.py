b1047b042ee8212b73f1f35bfbed8210fb29ec73
PranjalChaudhary <pranjal15lp@gmail.com>
2022-06-07 13:33:28 +0530

    second commit

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from turtle import Screen
 from paddle import Paddle
 
 screen = Screen()
-
+screen.tracer(0)
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.listen()
-player1 = Paddle(.98)
-player2 = Paddle(-1)
+player1 = Paddle(384,0)
+player2 = Paddle(-390,0)
 
 screen.onkeypress(player1.move_up, "Up")
 screen.onkeypress(player1.move_down, "Down")
@@ -16,5 +16,9 @@ screen.onkeypress(player1.move_down, "Down")
 screen.onkeypress(player2.move_up, "w")
 screen.onkeypress(player2.move_down, "s ")
 
+game_is_on = True
+
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,22 +1,24 @@
 from turtle import Turtle
 
 
-class Paddle:
-    def __init__(self, sign):
+class Paddle(Turtle):
+    def __init__(self, x_cor, y_cor):
+        super().__init__()
         self.paddle = Turtle()
-        self.paddle.shape('square')
-        self.paddle.color('white')
-        self.paddle.shapesize(stretch_len=1, stretch_wid=5)
-        self.paddle.penup()
+        self.xcor = x_cor
+        self.ycor = y_cor
+        self.shape('square')
+        self.color('white')
+        self.shapesize(stretch_len=1, stretch_wid=5)
+        self.penup()
         self.new_y = 0
-        self.sign = sign
-        self.paddle.speed("fastest")
-        self.paddle.goto(self.sign * 390, 100)
+        self.speed("fastest")
+        self.goto(self.xcor, self.ycor)
 
     def move_up(self):
-        self.new_y = self.paddle.ycor() + 20
-        self.paddle.goto(self.paddle.xcor(), self.new_y)
+        self.new_y = self.ycor() + 20
+        self.goto(self.xcor(), self.new_y)
 
     def move_down(self):
-        self.new_y = self.paddle.ycor() - 20
-        self.paddle.goto(self.paddle.xcor(), self.new_y)
+        self.new_y = self.ycor() - 20
+        self.goto(self.xcor(), self.new_y)
